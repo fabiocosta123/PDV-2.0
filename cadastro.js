db3344e527b658d7dfdf872c0ebdf6a2bdacbea4
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-03-27 06:19:58 -0300

    desenvolvendo função cadastrar

---
+++ cadastro.js
@@ -0,0 +1,7 @@
+localStorage.setItem('cadastro', 'pessoas')
+
+function cadastrar(){
+    let nome = document.getElementById('nome')
+    let usuario = document.getElementById('usuario')
+    let senha = document.getElementById('senha')
+}
