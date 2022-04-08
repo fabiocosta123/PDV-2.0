827d9c59483f2af2efb2cdfa1c80c2afc007f593
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-04-08 00:49:12 -0300

    criando função de gravar dados no localstorage

--- cadastro.js
+++ cadastro.js
@@ -1,12 +1,8 @@
 let nome = document.querySelector('#nome')
 let usuario = document.querySelector('#usuario')
 let senha = document.querySelector('#senha')
-let confirmaSenha = document.querySelector('#confirmaSenha')
-
-nome.addEventListener('keyup', ()=> {
-    
-})
-
+let cadastro = { nome: nome.value, usuario: usuario.value, senha: senha.value}
 function cadastrar(){
-
+  
 }
+
