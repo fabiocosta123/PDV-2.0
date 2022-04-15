1fc45a80134415ccaf95291af80f0ce55e8297dd
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-04-15 03:24:40 -0300

    revendo codigo

--- js/login.js
+++ js/login.js
@@ -1,30 +1,9 @@
-/*import * as cadastrar from "cadastro.js";
-let usuario = document.querySelector('#usuario')
-let senha = document.querySelector('#senha')
-
-
-const newLocal = localStorage.getItem(cad)
-console.log(newLocal);
-function login(){
-    if(usuario.value || senha.value != newLocal.usuario || newLocal.senha){
-        alert('Dados incorretos, verifique os dados')
-    } else {
-        alert('tudo certo')
-    }
-
- 
-}*/
 
 function login() {
     let usuario = document.querySelector('#usuario')
     let senha = document.querySelector('#senha')
     let listaUsuario = []
-    let user = {
-        nome: '',
-        usuario: '',
-        senha: ''
-    }
-
+   
     listaUsuario = JSON.parse(localStorage.getItem('cad'))
 
     listaUsuario.forEach((element) => {
