298ece4a0c16eedc1c2abf3d71029e2f9b439332
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-04-15 02:38:15 -0300

    verificando dados com localstorage para login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-import * as cadastrar from "cadastro.js";
+/*import * as cadastrar from "cadastro.js";
 let usuario = document.querySelector('#usuario')
 let senha = document.querySelector('#senha')
 
@@ -13,7 +13,29 @@ function login(){
     }
 
  
+}*/
+
+function login() {
+    let usuario = document.querySelector('#usuario')
+    let senha = document.querySelector('#senha')
+    let listaUsuario = []
+    let user = {
+        nome: '',
+        usuario: '',
+        senha: ''
+    }
+
+    listaUsuario = JSON.parse(localStorage.getItem('cad'))
+
+    listaUsuario.forEach((element) => {
+        if(usuario.value == element.usuario && senha.value == element.senha){
+            window.open('menu.html', '_self')
+        }else {
+            alert('Dados incorretos, verifique os dados digitados')
+        }
+    });
 }
+
 
 
 
