fa91e5979e0ebca870a1b2c20c24ac6f4338d9d2
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-04-28 01:05:55 -0300

    inlcuindo tela pagar.html (window.open) ao botao pagar na tela de venda

--- login.js
+++ login.js
@@ -1,21 +1,15 @@
-
 function login() {
-    let usuario = document.querySelector('#usuario')
-    let senha = document.querySelector('#senha')
-    let listaUsuario = []
-   
-    listaUsuario = JSON.parse(localStorage.getItem('cad'))
+  let usuario = document.querySelector("#usuario");
+  let senha = document.querySelector("#senha");
+  let listaUsuario = [];
 
-    listaUsuario.forEach((element) => {
-        if(usuario.value == element.usuario && senha.value == element.senha){
-            window.open('menu.html', '_self')
-        }else {
-            alert('Dados incorretos, verifique os dados digitados')
-        }
-    });
+  listaUsuario = JSON.parse(localStorage.getItem("cad"));
+
+  listaUsuario.forEach((element) => {
+    if (usuario.value == element.usuario && senha.value == element.senha) {
+      window.open("menu.html", "_self");
+    } else {
+      alert("Dados incorretos, verifique os dados digitados");
+    }
+  });
 }
-
-
-
-
-
