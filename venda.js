fa28852b5b96126ad775d62b706661aa275d6e87
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-05-02 01:25:38 -0300

    valores acrescidos a nota de rodapé <total a pagar>

--- venda.js
+++ venda.js
@@ -1,15 +1,19 @@
-import {arrayProdutos} from "./cadastroProduto"
+localstorage.getItem(produtos);
+
 function incluirVenda() {
-    let quantidade = document.querySelector('#quantidade')
-    let codigo = document.querySelector('#codigo')
-    let nomeProduto = document.querySelector('#nomeProduto')
-    let preco = document.querySelector('#preco')
+  let quantidade = document.querySelector("#quantidade").value;
+  let codigo = document.querySelector("#codigo").value;
+  let nomeProduto = document.querySelector("#nomeProduto").value;
+  let preco = document.querySelector("#preco").value;
+  let incluirVenda = [];
+  let txtPagar = document.querySelector("#totalPagar");
 
-
-    let incluirVenda = []
-    incluirVenda = JSON.parse(localStorage.getItem('arrayProdutos'))
-   
-    incluirVenda.forEach(element => {
+  let pagar = quantidade * preco;
+  let tPagar = 0;
+  tPagar += pagar;
+  txtPagar.innerHTML = `Total a pagar R$ ${tPagar.toFixed(2)}`;
+}
+/*  incluirVenda.forEach(element => {
         if (codigo.value == element.id) {
             function produto() {                               //colocar de forma dinamica um valor em um campo
               let recebeProduto = element.nameProduto
@@ -23,5 +27,4 @@ function incluirVenda() {
         }
     });
 }
-
-
+*/
